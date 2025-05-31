182cddf086aa2078bc821ff7bd232c08692290d7
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-31 17:23:42 -0400

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+function orderPizza(name) {
+    if (!name) {
+        throw new Error("Name is required to order a pizza.");  
+    }
+    return `Thanks, ${name}! Your pizza is on the way!`;
+}
+
+document.getElementById('orderBtn').addEventListener('click', () => {
+    const name = docuement.getElementById('nameInput').value;
+    const messageEl = document.getElementById("message");
+
+    try {
+        const msg = orderPizza(name);
+        messageEl.textContent = msg;
+        messageEl.style.color = "green";
+    } catch (error) {
+        messageEl.textContent = error.message;
+        messageEl.style.color = "red";      
+    }
+})
